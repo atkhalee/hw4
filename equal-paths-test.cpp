d2aab510376685cc41682b49f4be0b088ddadc89
Aamina Thasneem Khaleel <atkhalee@usc.edu>
2022-11-04 02:21:29 +0000

    Completed Problem 2

--- equal-paths-test.cpp
+++ equal-paths-test.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <cstdlib>
 #include "equal-paths.h"
+//#include "equal-paths.h"
 using namespace std;
 
 
@@ -10,6 +11,7 @@ Node* c;
 Node* d;
 Node* e;
 Node* f;
+Node* g;
 
 void setNode(Node* n, int key, Node* left=NULL, Node* right=NULL)
 {
@@ -54,23 +56,82 @@ void test5(const char* msg)
   setNode(d,4,NULL,NULL);
   cout << msg << ": " <<   equalPaths(a) << endl;
 }
+/*
+//Node* n, int key, Node* left=NULL, Node* right=NULL
+void test6(const char* msg)
+{
+  setNode(a,5,b,c);
+  setNode(b,1,d,e);
+  setNode(c,8,NULL,NULL);
+  setNode(d,0,NULL,NULL);
+	setNode(e,4,NULL,NULL);
+  cout << msg << ": " <<   equalPaths(a) << endl;
+}//needs to return false ==> 0
 
+void test7(const char* msg)
+{
+  setNode(a,5,b,c);
+  setNode(b,1,d,e);
+  setNode(c,8,f,NULL);
+  setNode(d,0,NULL,NULL);
+	setNode(e,4,NULL,NULL);
+	setNode(f,7,NULL,NULL);
+  cout << msg << ": " <<   equalPaths(a) << endl;
+}//needs to retrurn true --> 1
+
+void test8(const char* msg)
+{
+  setNode(a,5,b,c);
+  setNode(b,1,d,e);
+  setNode(c,8,f,NULL);
+  setNode(d,0,NULL,NULL);
+	setNode(e,4,f,NULL);
+	setNode(f,7,NULL,NULL);
+	setNode(g, 2,NULL,NULL);
+  cout << msg << ": " <<   equalPaths(a) << endl;
+}//needs to retrurn false --> 0
+
+void test9(const char* msg){
+	setNode(a,5,b,c);
+  setNode(b,1,d,NULL);
+  setNode(c,8,e,NULL);
+  setNode(d,0,NULL,NULL);
+	setNode(e,7, NULL,NULL);
+	cout << msg << ": " <<   equalPaths(a) << endl;
+}//needs to return true-->1
+*/
+//Tests 6-9 were based on the examples provided in problem 2. I tested this for my own understanding and to check that my code.
 int main()
 {
   a = new Node(1);
   b = new Node(2);
   c = new Node(3);
   d = new Node(4);
+	//Nodes for tests 6-9
+	// a = new Node(5);
+  // b = new Node(1);
+  // c = new Node(8);
+  // d = new Node(0);
+	// e = new Node(4);
+	// f = new Node (7);
+	// g= new Node (2);
 
   test1("Test1");
   test2("Test2");
   test3("Test3");
   test4("Test4");
   test5("Test5");
+	// test6("Test6");
+	// test7("Test7");
+	// test8("Test8");
+	// test9("Test9");
  
   delete a;
   delete b;
   delete c;
   delete d;
+	// delete e;
+	// delete f;
+	// delete g;
 }
 
--- equal-paths.cpp
+++ equal-paths.cpp
@@ -1,13 +1,72 @@
 #include "equal-paths.h"
+#include <cmath>
 using namespace std;
-
+#include <iostream>
 
 // You may add any prototypes of helper functions here
 
+//need to check the size of the path
+//if the depth is the same for the left and the right side-> then return true
+//else return false
+//need to recurse both the left and the right paths for this to work
+//it also needs to be recursive
+
+int CalculatingTheDepth(Node * root){
+    
+		int depth=0; //initializing depth to 0.
+    if(root == NULL){return depth;} //base case (based on recursion lecture slides)
+		depth+=1;
+		//need to traverse both the left and right subtrees. I determined to focus on travesing just the left subtree and keeping the right subtree as an edge case (where a left node has a right subtree)
+		if(root->right!=NULL){
+			return depth+CalculatingTheDepth(root->right);
+		}
+		return depth + CalculatingTheDepth(root->left);
+		//code that actually worked for me the first time (except for one of the edge cases)
+		/*
+		when depth = 0;
+		depth +=1;
+		return depth + CalculatingTheDepth(root->left);
+		*/
+		
+		// CalculatingTheDepth(root->left);
+		// CalculatingTheDepth(root->right);
+		// return depth;
+		// if(root->left!=NULL){
+		// 	return depth + CalculatingTheDepth(root->left);
+		// }else{
+		// 	return depth + CalculatingTheDepth(root->right);
+		// }
+		
+    
+}
 
 bool equalPaths(Node * root)
 {
     // Add your code below
+		//created three variables
+		//one for the rootDepth (set to 1 as root depth is always 1), and the other two for the left adn right Depths
+		int rootDepth =1;
+    int leftDepth=0;
+    int rightDepth=0;
+		//two if statements that check if the root node has and left or right childs
+		//if so, each designated variables (leftDepth or rightDepth) calls the Calculating Depth function
+		if(root->left!=NULL){
+    	leftDepth = rootDepth + CalculatingTheDepth(root->left);
+			//cout<<"leftDepth: " << leftDepth<<endl;
+			//used the cout statements to check the calculations of the depths
+		}
+		if(root->right!=NULL){
+    	rightDepth = rootDepth + CalculatingTheDepth(root->right);
+			//cout<<"rightDepth: " << rightDepth<<endl;
+		}
+		//checking if the two variables are the same depth
+		//reason I set left and right Depth variables to 0 is because of the Test1 in equal-paths-test.cpp file
+    if(leftDepth == rightDepth){
+        return true;
+    }else{
+        return false;
+    }
+		//return false;
 
 }
 
